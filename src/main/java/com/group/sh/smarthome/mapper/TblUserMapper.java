a602745a0659d4435aed2e7f8adfb81b41b70093
Mr.Fan <https://github.com/FanZhiQingGitHub/smarthome.git>
2020-11-27 22:03:07 +0800

    智慧家庭信息系统项目结构

--- src/main/java/com/group/sh/smarthome/mapper/TblUserMapper.java
+++ src/main/java/com/group/sh/smarthome/mapper/TblUserMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.group.sh.smarthome.entity.TblUser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -14,5 +16,13 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface TblUserMapper extends BaseMapper<TblUser> {
+
+    public List<TblUser> getUserInfoList(TblUser tblUser);
+
+    public String addUserInfo(List<TblUser> tblUserList);
+
+    public Boolean updateUserInfo(TblUser tblUser);
+
+    public Boolean deleteUserInfo(TblUser tblUser);
 
 }
